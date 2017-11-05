d17f1ea109528cc626107899ea429e76bd1ac058
ThaboMoopa <thabo.moopa@gmail.com>
2017-11-05 17:30:33 +0200

    prevent inserting new admin fixed

--- js/Login.js
+++ js/Login.js
@@ -1,18 +1,42 @@
 $(document).ready(function() {
     var URLlink = "http://localhost:8080";
     //load data to the user table
+    var name = "admin@vehicleRental.com";
+    var email = '';
     $.ajax({
-        type: "POST",
+        type: "GET",
         dataType: "json",
-        url: URLlink + "/user/addUser?",
-        data: "name=admin@vehicleRental.com&surname=admin&password=admin123&role=admin",
+        url: URLlink + "/user/findByName?",
+        data: "email=admin@vehicleRental.com",
         async: false,
         success: function (response) {
-
+            console.log(response);
+            email = response.name;
         }
-
     });
+
+    if (email == name) {
+        //alert("the user already exists");
+        event.preventDefault();
+        return;
+    }
+    else
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: URLlink + "/user/addUser?",
+            data: "name=admin@vehicleRental.com&surname=admin&password=admin123&role=admin",
+            async: false,
+            success: function (response) {
+                console.log("added new");
+                event.preventDefault();
+            }
+
+        });
 });
+
+
+
 //$(document).ready(function(){
 
 
