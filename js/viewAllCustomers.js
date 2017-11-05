f308bbbb7cb843d1b4e30242381c89a77d19fc19
Jean-PaulD <jpdebuys@gmail.com>
2017-11-05 13:22:53 +0200

    Delete Customer

--- js/viewAllCustomers.js
+++ js/viewAllCustomers.js
@@ -11,9 +11,27 @@ $(document).ready(function() {
                 "<td>"+  data[index].surname +" </td> " +
                 "<td>"+  data[index].email +" </td> " +
                 "<td> <a href='updateCustomer.html?customerID="+data[index].id+"' > Update Customer </a> </td> " +
+                "<td  id='deleteCustomer'> " +
+                "<input class=\"btn btn-outline-success\" type='button' value='Delete Customer' onclick='deleteCustomer("+data[index].id+"); return false;'></input> </td> " +
                 "</tr>")
                 .prependTo(".customersTable")
                 //alert(data[index].name);
         });
     });
+
 });
+function deleteCustomer(customerID){
+    aler
+    $.getJSON('http://localhost:8080/customer/deleteCustomer?customerID='+customerID+'', function (data) {
+        $.each(data, function (index) {
+            //alert(data[index].id);
+
+            if(data[index].email == ""){
+                alert("Customer deleted");
+            }
+            else {
+                alert("deleting customer failed");
+            }
+        });
+    });
+}
